f89bbd1abed3b09859c941fc8eee4cd7ecd28ef3
Kaylen Pillay <kaylen.pillay@softwareological.com>
2024-12-23 20:42:51 +0200

    feat: Top App Bar
    1. Added the top app bar shared component Composable. The composable supports
    a title and a navigation icon.
    
    Github Task Ticket:
    https://github.com/KaylenTPillay/Expense-Tracker-App/issues/12
    
    Signed-off-by: Kaylen Pillay <kaylen.pillay@softwareological.com>

---
+++ app/src/main/java/com/kaylen/pillay/expensetracker/ui/view/sharedcomponent/topappbar/TopAppBarSharedComponent.kt
@@ -0,0 +1,93 @@
+package com.kaylen.pillay.expensetracker.ui.view.sharedcomponent.topappbar
+
+import android.content.res.Configuration
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kaylen.pillay.expensetracker.ui.theme.ExpenseTrackerTheme
+import com.kaylen.pillay.expensetracker.ui.view.sharedcomponent.topappbar.event.TopAppBarSharedEventContract
+import com.kaylen.pillay.expensetracker.ui.view.sharedcomponent.topappbar.state.TopAppBarSharedStateModel
+
+/*
+ * Designed and developed by Kaylen Travis Pillay.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarSharedComponent(
+    state: TopAppBarSharedStateModel,
+    eventContract: TopAppBarSharedEventContract?,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Text(text = state.title)
+        },
+        navigationIcon = {
+            TopAppBarNavigationIcon(
+                displayNavigationIcon = state.navigationIcon,
+                eventContract = eventContract
+            )
+        }
+    )
+}
+
+@Composable
+private fun TopAppBarNavigationIcon(
+    displayNavigationIcon: Boolean,
+    eventContract: TopAppBarSharedEventContract?
+) {
+    if (!displayNavigationIcon) return
+
+    IconButton(onClick = { eventContract?.onTopAppBarNavigationIconClick() }) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            contentDescription = "Back Navigation Button"
+        )
+    }
+}
+
+private val previewState = TopAppBarSharedStateModel(
+    title = "Expense Tracker App",
+    navigationIcon = true
+)
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewTopAppBarSharedComponent_Night() {
+    val previewEventContract: TopAppBarSharedEventContract? = null
+
+    ExpenseTrackerTheme {
+        TopAppBarSharedComponent(previewState, previewEventContract)
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun PreviewTopAppBarSharedComponent_Day() {
+    val previewEventContract: TopAppBarSharedEventContract? = null
+
+    ExpenseTrackerTheme {
+        TopAppBarSharedComponent(previewState, previewEventContract)
+    }
+}
